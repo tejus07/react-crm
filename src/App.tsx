7a2e3064d7492fe7b34135df071323439e0b0ca6
Tejus Sahi <tejussahi07@gmail.com>
2024-08-04 17:44:24 +0530

    Added Side bar and nav bar UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,21 @@
 import './App.css'
-import { Button } from "@/components/ui/button"
-import { UserNav } from './components/user-nav'
-import { MainNav } from './components/MainNav'
-import { Search } from './components/Search'
-import { ThemeProvider } from "@/components/theme-provider"
-import { Avatar, AvatarFallback, AvatarImage } from './components/ui/avatar'
+import { ThemeProvider } from "@/components/ThemeProvider.tsx"
+import {NavBar} from "@/components/NavBar.tsx";
+import {SideBar} from "@/components/SideBar.tsx";
+import {DashBoard} from "@/components/DashBoard.tsx";
 
 function App() {
 
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <div className="hidden flex-col md:flex custom-background">
-          <div className="border-b">
-            <div className="flex h-16 items-center px-4">
-      
-                  <Avatar className="h-8 w-8">
-                    <AvatarFallback>N</AvatarFallback>
-                  </Avatar>
-            
-              <Search />
-              <div className="ml-auto flex items-center space-x-4">
-                <Button variant="ghost" className="relative h-8 w-8 rounded-full overflow:hidden">
-                  <Avatar className="h-8 w-8">
-                    {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
-                    <AvatarFallback>M</AvatarFallback>
-                  </Avatar>
-                </Button>
-                <Button variant="ghost" className="relative h-8 w-8 rounded-full overflow:hidden">
-                  <Avatar className="h-8 w-8">
-                    {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
-                    <AvatarFallback>S</AvatarFallback>
-                  </Avatar>
-                </Button>
-                <Button variant="ghost" className="relative h-8 w-8 rounded-full overflow:hidden">
-                  <Avatar className="h-8 w-8">
-                    {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
-                    <AvatarFallback>N</AvatarFallback>
-                  </Avatar>
-                </Button>
-                <UserNav />
+        <NavBar/>
+          <div className="flex h-screen">
+              <SideBar/>
+              <div className="w-full">
+                  <DashBoard/>
               </div>
-            </div>
           </div>
-        </div>
       </ThemeProvider>
     </>
   )
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,8 +2,8 @@ import { Input } from "@/components/ui/input"
 
 export function Search() {
   return (
-    <div className="search-container relative">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search absolute left-2 top-2.5 h-4 w-4"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg>
+    <div className="search-container relative mx-10">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search absolute left-2 top-2.5 h-4 w-4"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg>
       <Input
         type="search"
         placeholder="Search"
